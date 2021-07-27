870dce9f18088d47a2662fadbd5eefe295c6d46e
mrgiulls <giuliano.mesias@hotmail.com>
2021-07-27 16:53:34 -0500

    Se hizo un nuevo archivo para pruebas

---
+++ tests/main1.c
@@ -0,0 +1,32 @@
+#include <limits.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int len;
+	int len2;
+
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+	_printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);
+	_printf("INT_MAX:[%d]\n", INT_MAX);
+	printf("INT_MAX:[%d]\n", INT_MAX);
+        _printf("INT_MIN:[%d]\n", INT_MIN);
+        printf("INT_MIN:[%d]\n", INT_MIN);
+	_printf("Negative:[%d]\n", -762534);
+	printf("Negative:[%d]\n", -762534);
+	len = _printf("Percent:[%%]\n");
+	len2 = printf("Percent:[%%]\n");
+	_printf("Len:[%d]\n", len);
+	printf("Len:[%d]\n", len2);
+	_printf("Unknown:[%r]\n");
+	printf("Unknown:[%r]\n");
+	return (0);
+}
