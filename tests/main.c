12506ff875e23d1ac7604ef4556bb6f25f102391
mrgiulls <giuliano.mesias@hotmail.com>
2021-07-27 00:18:00 -0500

    agregar print al octal

--- tests/main.c
+++ tests/main.c
@@ -10,8 +10,11 @@ int main(void)
 {
 	int len;
 	int len2;
+	unsigned int ui;
+	
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
+	ui = (unsigned int)INT_MAX + 1024;
 	_printf("Character:[%c]\n", 'H');
 	printf("Character:[%c]\n", 'H');
 	_printf("Character:[%c]\n", 'H');
@@ -27,5 +30,7 @@ int main(void)
 	_printf("[%u]\n", -500);
 	printf("[%u]\n", -500);
 	_printf("[%u]\n", 500);
+	_printf("Unsigned octal:[%o]\n", ui);
+	printf("Unsigned octal:[%o]\n", ui);
 	return (0);
 }
